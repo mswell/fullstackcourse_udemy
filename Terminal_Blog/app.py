4b6d662c5fc8f1f4809f925a2211d8b7a751544c
Wellington Moraes <wellpunk@gmail.com>
2017-02-01 09:02:38 -0200

    Change

--- Terminal_Blog/app.py
+++ Terminal_Blog/app.py
@@ -1,9 +1,18 @@
 from database import Database
-from menu import Menu
+from models.blog import Blog
 
 
 Database.initialize()
 
-menu = Menu()
+blog = Blog(
+        author="Wellington",
+        title="Sample title",
+        description="Sample description")
 
-menu.run_menu()
+blog.new_post()
+
+blog.save_to_mongo()
+
+from_database = Blog.from_mongo(blog.id)
+
+print(blog.get_posts())
